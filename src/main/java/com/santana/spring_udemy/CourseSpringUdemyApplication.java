03bf62b18d6fe5d46d1078c53a575e86f132538c
Eder Sant'ana <edersana@gmail.com>
2021-02-02 11:31:38 -0300

    Project created

---
+++ src/main/java/com/santana/spring_udemy/CourseSpringUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.santana.spring_udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santana/spring_udemy/CourseSpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santana.spring_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
