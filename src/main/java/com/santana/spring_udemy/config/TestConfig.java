b32742308b60e22d759e15a0593f24d608e9f6f9
Eder Sant'ana <edersana@gmail.com>
2021-02-04 18:23:52 -0300

    Payment ont-to-one association

--- src/main/java/com/santana/spring_udemy/config/TestConfig.java
+++ src/main/java/com/santana/spring_udemy/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.santana.spring_udemy.entities.Category;
 import com.santana.spring_udemy.entities.Order;
 import com.santana.spring_udemy.entities.OrderItem;
+import com.santana.spring_udemy.entities.Payment;
 import com.santana.spring_udemy.entities.Product;
 import com.santana.spring_udemy.entities.User;
 import com.santana.spring_udemy.entities.enums.OrderStatus;
@@ -34,7 +35,6 @@ public class TestConfig implements CommandLineRunner{
 	private ProductRepository productRepository;
 	@Autowired
 	private OrderItemRepository orderItemRepository;
-	
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -83,6 +83,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+	
 		
 	}
 
