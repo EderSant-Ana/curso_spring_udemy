065ebb6f3dc7747a283c4b065a8dd0f0f7af60f3
Eder Sant'ana <edersana@gmail.com>
2021-02-05 17:18:52 -0300

    Dev profile

--- src/main/java/com/santana/spring_udemy/resources/ProductResource.java
+++ src/main/java/com/santana/spring_udemy/resources/ProductResource.java
@@ -1,13 +1,19 @@
 package com.santana.spring_udemy.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.santana.spring_udemy.entities.Product;
 import com.santana.spring_udemy.services.ProductService;
@@ -30,5 +36,23 @@ public class ProductResource {
 		Product obj = productService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
+	@PostMapping
+	public ResponseEntity<Product> save(@RequestBody Product obj){
+		obj = productService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		productService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product obj){
+		obj = productService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
--- src/main/java/com/santana/spring_udemy/services/ProductService.java
+++ src/main/java/com/santana/spring_udemy/services/ProductService.java
@@ -3,11 +3,17 @@ package com.santana.spring_udemy.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.santana.spring_udemy.entities.Product;
 import com.santana.spring_udemy.repositories.ProductRepository;
+import com.santana.spring_udemy.services.exceptions.DatabaseException;
+import com.santana.spring_udemy.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -22,7 +28,34 @@ public class ProductService {
 	
 	public Product findById(Long id) {
 		Optional<Product> obj = productRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Product insert(Product obj) {
+		return productRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			productRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}				
+	}
+	
+	public Product update (Long id, Product obj) {
+		Product entity = productRepository.getOne(id);
+		try {
+			entity.setName(obj.getName());
+			entity.setDescription(obj.getDescription());
+			entity.setPrice(obj.getPrice());
+			entity.setImgUrl(obj.getImgUrl());
+			return productRepository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 }
